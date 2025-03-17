ce871fe8a1158a5ef0722ddbbb6d39f96edcd3eb
noumankhan20 <noumankhan8097@gmail.com>
2025-03-17 17:25:27 +0530

    updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,12 @@
 import React from 'react'
 import Navbar from '../components/layout/navbar.jsx' // Remove .jsx if using Next.js 13+
 import Footer from '../components/layout/footer.jsx'
+import Home from '../components/Home/home.jsx'
 const Page = () => {
   return (
     <div>
       <Navbar />
+      <Home />
       <Footer />
     </div>
   )
